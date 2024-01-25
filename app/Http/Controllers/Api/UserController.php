89da543e9d468c98d41a18861e4c3c1365f9a0b7
MarkosMartinez <ikbdk@plaiaundi.net>
2024-01-26 00:38:19 +0100

    fallo al recibir el "nuevo" json arreglado

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
 
     return response()->json([
         'success' => true,
-        'message' => 'Ubicaciones guardadas correctamente: '.$ubicaciones
+        'message' => 'Ubicaciones guardadas correctamente'
     ]);
 }
 
